eef9d7a874c6634f87c51d640e7c751928e97fea
Milutin Nikolic <ajsmilutin@gmail.com>
2023-10-27 16:59:59 +0200

    Started stepping

--- visualize.py
+++ visualize.py
@@ -1,41 +1,54 @@
 import time
 
+import numpy as np
 import mujoco
 import mujoco.viewer
+from controllers.controller import Controller
+from utils.visualization_utils import draw_frame
 
 m = mujoco.MjModel.from_xml_path('solo_model.xml')
 d = mujoco.MjData(m)
 
-total_time = 10
+robot_controler = Controller()
+
 def controller(model, data):
-    global total_time
-    # put the controller here. This function is called inside the simulation.
-    data.ctrl[1] = 1.047
-    data.ctrl[2] = -1.57
-    data.ctrl[4] = 1.047
-    data.ctrl[5] = -1.57
-    data.ctrl[7] = 1.047
-    data.ctrl[8] = -1.57
-    data.ctrl[10] = 1.57
-    data.ctrl[11] = -2
-    data.ctrl = min(data.time, total_time) / total_time * data.ctrl
+    q = data.qpos[7:]
+    qv = data.qvel[6:]
+    touch_sensors = ["fl", "fr", "hl", "hr"]
+    reading = {}
+    for sensor in touch_sensors:
+        name = sensor + "_touch"
+        reading[name] = data.sensor(name).data[0]>0
+    sensor = {"imu": (data.sensor("angular-velocity").data, data.sensor(
+        "linear-acceleration").data, data.sensor("magnetometer").data),
+        "touch": reading}
+    data.ctrl = robot_controler.compute_controll(data.time, q, qv, sensor)
 
 
-with mujoco.viewer.launch_passive(m, d) as viewer:
-  # Close the viewer automatically after 30 wall-seconds.
-  start = time.time()
-  mujoco.set_mjcb_control(controller)
+def update_scene(scn, model, data):
+    rot = np.zeros((9,), dtype=np.float64)
+    scn.ngeom = 0
+    mujoco.mju_quat2Mat(rot, robot_controler.Q)
+    draw_frame(scn, robot_controler.pos, rot.reshape((3, 3)), 0.005, 0.2)
 
-  while viewer.is_running() and time.time() - start < 30:
-    step_start = time.time()
-    # mj_step can be replaced with code that also evaluates
-    # a policy and applies a control signal before stepping the physics.
-    mujoco.mj_step(m, d)
-  
-    # Pick up changes to the physics state, apply perturbations, update options from GUI.
-    viewer.sync()
 
-    # Rudimentary time keeping, will drift relative to wall clock.
-    time_until_next_step = m.opt.timestep - (time.time() - step_start)
-    if time_until_next_step > 0:
-      time.sleep(time_until_next_step)
+with mujoco.viewer.launch_passive(m, d, show_right_ui=False) as viewer:
+    # Close the viewer automatically after 30 wall-seconds.
+    start = time.time()
+    mujoco.set_mjcb_control(controller)
+
+
+    while viewer.is_running() and time.time() - start < 30:
+        step_start = time.time()
+        # mj_step can be replaced with code that also evaluates
+        # a policy and applies a control signal before stepping the physics.
+        mujoco.mj_step(m, d)
+
+        # Pick up changes to the physics state, apply perturbations, update options from GUI.
+        viewer.sync()
+        update_scene(viewer.user_scn, m, d)
+
+        # Rudimentary time keeping, will drift relative to wall clock.
+        time_until_next_step = m.opt.timestep - (time.time() - step_start)
+        if time_until_next_step > 0:
+            time.sleep(time_until_next_step)
