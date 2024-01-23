cb682c0e6561cd5c7e43a0f76395aca9f797c124
Milutin Nikolic <ajsmilutin@gmail.com>
2024-01-23 01:38:11 +0100

    Added connector and moved files to ftn_solo

--- ftn_solo/visualize.py
+++ ftn_solo/visualize.py
@@ -1,20 +1,32 @@
+#!/usr/bin/env python
 import time
 
 import numpy as np
 import mujoco
 import mujoco.viewer
-from controllers.controller import Controller
-from utils.visualization_utils import draw_frame, draw_surface
+# from controllers.controller import Controller
+# from utils.visualization_utils import draw_frame, draw_surface
 
 paused = False
 leave = False
-
-robot_controler = Controller()
-m = mujoco.MjModel.from_xml_path(robot_controler.get_mjcf())
-d = mujoco.MjData(m)
-
+# robot_controler = Controller()
 
 def controller(model, data):
+    des_pos = np.array(
+    [
+        0.3,   # FLHAA
+        0.7,   # FLHFE
+        -1.57,  # FLKFE
+        -0.3,  # FRHAA
+        0.7,   # FRHFE
+        -1.57,  # FRKFE
+        0.3,   # FLHAA
+        0.7,   # FLHFE
+        -1.57,  # FLKFE
+        -0.3,  # FRHAA
+        0.7,   # FRHFE
+        -1.57,  # FRKFE
+    ])
     q = data.qpos[7:]
     qv = data.qvel[6:]
     touch_sensors = ["fl", "fr", "hl", "hr"]
@@ -25,16 +37,15 @@ def controller(model, data):
     sensor = {"imu": (data.sensor("angular-velocity").data, data.sensor(
         "linear-acceleration").data, data.sensor("magnetometer").data),
         "touch": reading}
-    data.ctrl = robot_controler.compute_controll(data.time, q, qv, sensor)
-
+    data.ctrl = 1.0 * (des_pos - q) - 0.001 * qv
 
 def update_scene(scn, model, data):
     rot = np.zeros((9,), dtype=np.float64)
     scn.ngeom = 0
-    mujoco.mju_quat2Mat(rot, robot_controler.Q)
-    draw_frame(scn, robot_controler.pos, rot.reshape((3, 3)), 0.005, 0.2)
-    if robot_controler.surface:
-        draw_surface(scn, robot_controler.surface.position, robot_controler.surface.R, 1)
+    # mujoco.mju_quat2Mat(rot, robot_controler.Q)
+    # draw_frame(scn, robot_controler.pos, rot.reshape((3, 3)), 0.005, 0.2)
+    # if robot_controler.surface:
+    #     draw_surface(scn, robot_controler.surface.position, robot_controler.surface.R, 1)
 
 
 def key_callback(keycode):
@@ -45,12 +56,16 @@ def key_callback(keycode):
         global leave
         leave = True
 
-
+# def simulate():
+m = mujoco.MjModel.from_xml_path("/home/ajsmilutin/solo_ws/src/robot_properties_solo/src/robot_properties_solo/resources/mjcf/solo12.mjcf")
+m.body("base").jntnum = 0
+print(dir(m.body("base")))
+d = mujoco.MjData(m)
 with mujoco.viewer.launch_passive(m, d, show_right_ui=False, key_callback=key_callback) as viewer:
     mujoco.set_mjcb_control(controller)
 
     # Close viewer on ESC keyboard press
-    while viewer.is_running() and not leave:
+    while viewer.is_running():
         if paused:
             continue
         step_start = time.time()
@@ -66,5 +81,13 @@ with mujoco.viewer.launch_passive(m, d, show_right_ui=False, key_callback=key_ca
         time_until_next_step = m.opt.timestep - (time.time() - step_start)
         if time_until_next_step > 0:
             time.sleep(time_until_next_step)
+        if leave:
+            viewer.close()
+        print("STOPPP")
+    
+        
+        
+    
+# if __name__ == '__main__':
+#     simulate()    
 
-del robot_controler
