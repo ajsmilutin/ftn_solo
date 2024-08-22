6d7dfcd140ff47c9e47fbac6ce7e14afcab554ba
Milutin Nikolic <ajsmilutin@gmail.com>
2024-08-22 08:28:23 +0500

    Made walker

--- ftn_solo/utils/trajectories.py
+++ ftn_solo/utils/trajectories.py
@@ -5,26 +5,29 @@ from std_msgs.msg import ColorRGBA
 from .conversions import ToPoint
 from copy import deepcopy
 from scipy.interpolate import CubicSpline
+import pinocchio as pin
 
 
 class SplineData:
     def __init__(self, yaml_config, num_joints, poses) -> None:
         self.durations = np.array(yaml_config["durations"])
-        self.poses = np.ndarray(
-            (len(self.durations), num_joints), dtype=np.float64)
+        self.poses = np.ndarray((len(self.durations), num_joints), dtype=np.float64)
         for i, pose_name in enumerate(yaml_config["poses"]):
             self.poses[i, :] = poses[pose_name]
 
 
 def poly5(t, t_start, t_end, logger=None):
     delta_t = t_end - t_start
-    tau = (t - t_start)/delta_t
+    tau = (t - t_start) / delta_t
     if logger is not None:
-        logger.error("TImes are tstart = {} tend = {} delta_t={} t={} tau={}".format(
-            t_start, t_end, delta_t, t, tau))
-    s = (((6*tau - 15)*tau)+10)*tau*tau*tau
-    sdot = (((30*tau - 60)*tau)+30)*tau*tau/delta_t
-    sddot = (((120*tau - 180)*tau)+60)*tau/delta_t/delta_t
+        logger.error(
+            "TImes are tstart = {} tend = {} delta_t={} t={} tau={}".format(
+                t_start, t_end, delta_t, t, tau
+            )
+        )
+    s = (((6 * tau - 15) * tau) + 10) * tau * tau * tau
+    sdot = (((30 * tau - 60) * tau) + 30) * tau * tau / delta_t
+    sddot = (((120 * tau - 180) * tau) + 60) * tau / delta_t / delta_t
     return s, sdot, sddot
 
 
@@ -35,9 +38,11 @@ class Trajectory:
         self.times = []
         self.start = None
         self.finished = False
+        self.end_time = None
 
     def set_start(self, t):
         self.start = t
+        self.end_time = self.start + self.times[-1]
 
     def add(self, position, t):
         self.positions.append(position)
@@ -61,41 +66,101 @@ class Trajectory:
             p, v, _ = self.get(time)
             marker.points.clear()
             marker.points.append(ToPoint(p))
-            marker.points.append(ToPoint(p + v*delta_t * 0.5))
+            marker.points.append(ToPoint(p + v * delta_t * 0.5))
             marker_array.markers.append(deepcopy(marker))
             marker.id = marker.id + 1
-        return marker_array        
-
-class PiecewiseLinear(Trajectory):
-
-    def __str__(self) -> str:
-        return "Trajecotry with {} points lasing {}".format(len(self.positions), self.times[-1])
-
-    def close_loop(self, t):
-        self.loop = True
-        self.add(self.positions[0], t)
+        return marker_array
 
     def get(self, t, logger=None):
         start = self.start if self.start is not None else 0
         if t < start:
-            return self.positions[0], 0*self.positions[0], 0*self.positions[0]
+            return self.start_point()
 
-        trel = (t-start)
+        trel = t - start
         if self.loop:
             trel = trel % self.times[-1]
 
         if trel > self.times[-1]:
             self.finished = True
-            return self.positions[-1], 0*self.positions[0], 0*self.positions[0]
+            return self.end_point()
 
+        return self.get_interpolated(trel, logger=logger)
+
+
+class ConstOrientation(Trajectory):
+    def __init__(self, rotation):
+        super().__init__()
+        self.rotation = rotation
+        self.finished = True
+        self.times = [0]
+
+    def start_point(self):
+        return self.rotation, np.zeros(3), np.zeros(3)
+
+    def end_point(self):
+        return self.start_point()
+
+    def get_interpolated(self, tre, logger=None):
+        return self.start_point()
+
+
+class PiecewiseLinear(Trajectory):
+
+    def __str__(self) -> str:
+        return "Trajecotry with {} points lasing {}".format(
+            len(self.positions), self.times[-1]
+        )
+
+    def close_loop(self, t):
+        self.loop = True
+        self.add(self.positions[0], t)
+
+    def start_point(self):
+        return self.positions[0], 0 * self.positions[0], 0 * self.positions[0]
+
+    def end_point(self):
+        return self.positions[-1], 0 * self.positions[0], 0 * self.positions[0]
+
+    def get_interpolated(self, trel, logger=None):
         segment = 0
-        while trel > self.times[segment+1]:
-            segment = segment+1
-
-        direction = self.positions[segment+1]-self.positions[segment]
+        while trel > self.times[segment + 1]:
+            segment = segment + 1
         s, sdot, sddot = poly5(
-            trel, self.times[segment], self.times[segment+1], logger)
-        return self.positions[segment]+s*direction, sdot * direction, sddot*direction
+            trel, self.times[segment], self.times[segment + 1], logger
+        )
+        return self.interpolate(segment, s, sdot, sddot)
+
+    def interpolate(self, segment, s, sdot, sddot):
+        direction = self.positions[segment + 1] - self.positions[segment]
+        return (
+            self.positions[segment] + s * direction,
+            sdot * direction,
+            sddot * direction,
+        )
+
+
+class PiecewiseLinearRotation(PiecewiseLinear):
+    def __str__(self) -> str:
+        return "Trajecotry with {} points lasing {}".format(
+            len(self.positions), self.times[-1]
+        )
+
+    def start_point(self):
+        return self.positions[0], np.zeros(3), np.zeros(3)
+
+    def end_point(self):
+        return self.positions[-1], np.zeros(3), np.zeros(3)
+
+    def interpolate(self, segment, s, sdot, sddot):    
+        axang = pin.log(
+            np.matmul(self.positions[segment].T, self.positions[segment + 1])
+        )
+        return (
+            np.matmul(self.positions[segment], pin.exp(s*axang)), 
+            sdot * np.matmul(self.positions[segment],axang),
+            sddot * np.matmul(self.positions[segment],axang),
+        )
+
 
 class SplineTrajectory(Trajectory):
     def __init__(self):
@@ -107,28 +172,26 @@ class SplineTrajectory(Trajectory):
 
     def add(self, position, t):
         super().add(position, t)
-        if (len(self.times)>=2):
-            self.spline = CubicSpline(np.hstack(self.times), np.vstack(self.positions))
+        if len(self.times) >= 2:
+            self.spline = CubicSpline(
+                np.hstack(self.times), np.vstack(self.positions), bc_type="clamped"
+            )
 
-    def get(self, t, logger=None):
-        start = self.start if self.start is not None else 0
-        if t < start:
-            return self.positions[0], 0*self.positions[0], 0*self.positions[0]
-        trel = (t-start)
-        if trel > self.times[-1]:
-            self.finished = True
-            return self.positions[-1], 0*self.positions[0], 0*self.positions[0]
-        return self.spline(t), self.spline(t,1), self.spline(t,2)
-    
+    def start_point(self):
+        return self.positions[0], 0 * self.positions[0], 0 * self.positions[0]
+
+    def end_point(self):
+        return self.positions[-1], 0 * self.positions[0], 0 * self.positions[0]
+
+    def get_interpolated(self, trel, logger=None):
+        return self.spline(trel), self.spline(trel, 1), self.spline(trel, 2)
+
 
 def create_square(position, u1, u2, distance, time):
     trajectory = PiecewiseLinear()
     trajectory.add(position, 0)
-    trajectory.add(position + u1 * distance, time/4)
-    trajectory.add(position + u1 * distance + u2*distance, time/2)
-    trajectory.add(position + u2*distance, 3*time/4)
+    trajectory.add(position + u1 * distance, time / 4)
+    trajectory.add(position + u1 * distance + u2 * distance, time / 2)
+    trajectory.add(position + u2 * distance, 3 * time / 4)
     trajectory.close_loop(time)
     return trajectory
-
-
-
