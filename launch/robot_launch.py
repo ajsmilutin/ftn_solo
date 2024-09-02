be7734442ebfb224a3d7d28902c72e2003d3d7eb
Milutin Nikolic <ajsmilutin@gmail.com>
2024-09-02 12:51:23 +0900

    Added IK solver

--- launch/robot_launch.py
+++ launch/robot_launch.py
@@ -38,6 +38,17 @@ def launch_setup(context, *args, **kwargs):
             ],
         ),
         Node(
+            namespace="ik",
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
+            output="screen",
+            parameters=[
+                {"robot_description": robot_desc, "use_sim_time": use_sim_time,
+                 "frame_prefix":"ik/"},
+            ],
+        ),
+        Node(
             package="rviz2",
             executable="rviz2",
             name="rviz2",
