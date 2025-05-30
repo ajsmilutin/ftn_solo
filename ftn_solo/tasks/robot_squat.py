0ed37f2f9ad96f7b399613c275928b7a5003f6aa
Aleksandar Lalić <lalic@groschopp.rs>
2025-05-30 19:44:01 +0200

    move_base

--- ftn_solo/tasks/robot_squat.py
+++ ftn_solo/tasks/robot_squat.py
@@ -15,7 +15,8 @@ class RobotMove(TaskBase):
         self.joint_controller = RneAlgorithm(
             num_joints, self.config["joint_controller"], robot_version, logger, dt)
 
-        self.splines = {leg: {"arc": {}, "line": {}}
+        self.splines = {leg: {"arc": {"x": None, "y": None, "z": None}, 
+                             "line": {"x": None, "y": None, "z": None}}
                         for leg in ["FL", "FR", "HL", "HR"]}
 
         self.steps = []
@@ -26,6 +27,13 @@ class RobotMove(TaskBase):
         self.backwards = False
         self.move_x = False
         self.move_y = False
+        self.rotate = False
+        self.backwards_x = False
+        self.backwards_y = False
+        self.backwards_rot = False
+        self.off_x = 0.0
+        self.off_y = 0.0
+        self.off_rot = 0.0
         self.off_time = time.time()
 
         self.ndq = np.zeros(18)
@@ -33,84 +41,106 @@ class RobotMove(TaskBase):
 
         self.logger = logger
         self.R_y = np.eye(3)
-        self.start_point = 0.1469
         self.off = 0.05
         self.old_msg = Twist()
-        self.define_splines(self.off)
+        
+        # Initialize splines at startup
+        self.define_splines(0.005, 0.005, 0.005)
         
     def define_movement(self, msg):
         if msg != self.old_msg:
+
             self.start = True
-            if msg.linear.x != 0:
-                self.backwards = msg.linear.x < 0
-                self.move_x = True
-                self.move_y = False
-                self.start_point = 0.196
-                self.off = np.clip(abs(msg.linear.x), 0.0, 0.08)
-            elif msg.linear.y != 0:
-                self.backwards = msg.linear.y < 0
-                self.move_y = True
-                self.move_x = False
-                self.start_point = 0.1469
-                self.off = np.clip(abs(msg.linear.y), 0.0, 0.04)
-            elif msg.angular.z != 0:
-                self.backwards = msg.angular.z < 0
-                self.move_x = False
-                self.move_y = False
-                self.start_point = 0.1469
-                self.off = np.clip(abs(msg.angular.z), 0.00, 0.08)
-            else:
+            self.off = 0.05
+            # Handle combined movements
+            self.move_x = msg.linear.x != 0
+            self.move_y = msg.linear.y != 0
+            self.rotate = msg.angular.z != 0
+            
+            # Set direction flags
+            self.backwards_x = msg.linear.x < 0 
+            self.backwards_y = msg.linear.y > 0 
+            self.backwards_rot = msg.angular.z < 0 
+            
+            # For backwards compatibility
+            self.backwards = self.backwards_x or self.backwards_y or self.backwards_rot
+            
+            # Calculate offsets for each direction
+            self.off_x = np.clip(abs(msg.linear.x), 0.0, 0.03) if self.move_x else 0.0
+            self.off_y = np.clip(abs(msg.linear.y), 0.0, 0.02) if self.move_y else 0.0
+            self.off_rot = np.clip(abs(msg.angular.z), 0.0, 0.02) if self.rotate else 0.0
+               
+            
+            if msg.linear.x == 0 and msg.linear.y == 0 and msg.angular.z == 0:
+                self.off_x, self.off_y, self.off_rot = 0.0, 0.0, 0.0
                 self.off = 0.0
                 self.off_time = time.time()
-
-            self.define_splines(self.off)
+                
+            self.logger.info(f"Updating splines: X={self.move_x}, Y={self.move_y}, Rot={self.rotate}, Off={self.off}")
+            self.define_splines(self.off_x, self.off_y, self.off_rot)
+            
             self.old_msg = msg
         else:
             pass
 
-    def define_splines(self, off):
-
+    def define_splines(self, off_x, off_y, off_rot):
         t_arc_points = np.array([0, 0.5, 1])
-        front = np.array(
-            [self.start_point-off,  self.start_point,  self.start_point+off])
-        zarc = np.array([-0.20, -0.15, -0.20])
-        back = np.array(
-            [self.start_point+off,  self.start_point,  self.start_point-off])
-        zline = np.array([-0.20, -0.20, -0.20])
-
-        x_f, x_b = (back, front) if self.backwards else (front, back)
-
+        if self.rotate:
+            off_y = off_rot
+        # X direction splines
+        front_x = np.array([0.196-off_x, 0.196, 0.196+off_x])
+        back_x = np.array([0.196+off_x, 0.196, 0.196-off_x])
+        
+        # Y direction splines - use smaller offsets for lateral movement
+        front_y = np.array([0.1469-off_y, 0.1469, 0.1469+off_y])
+        back_y = np.array([0.1469+off_y, 0.1469, 0.1469-off_y])
+        
+        # Z direction (height) splines
+        zarc = np.array([-0.20, -0.15, -0.20])  # Arc has foot clearance
+        zline = np.array([-0.20, -0.20, -0.20])  # Line stays on ground
+        
+        # Adjust direction based on backwards flags
+        x_f, x_b = (back_x, front_x) if self.backwards_x else (front_x, back_x)
+        y_f, y_b = (back_y, front_y) if self.backwards_y or self.backwards_rot else (front_y, back_y)
+        
         for leg in self.splines.keys():
-
-            if self.move_x:
-                if leg == "FL" or leg == "FR":
-                    x_arc = x_f
-                    x_line = x_b
-                else:
-                    x_arc = -x_b
-                    x_line = -x_f
-            elif self.move_y:
-                if leg == "FR" or leg == "HR":
-                    x_arc = -x_f
-                    x_line = -x_b
-                else:
-                    x_arc = x_b
-                    x_line = x_f
-            else:
+            # X movement splines
+           
+            if leg == "FL" or leg == "FR":  # Front legs
+                x_arc = x_f
+                x_line = x_b
+            else:  # Hind legs
+                x_arc = -x_b
+                x_line = -x_f
+        
+    
+            if leg == "FR" or leg == "HR":  # Right legs
+                y_arc = -y_f
+                y_line = -y_b
+            else:  # Left legs
+                y_arc = y_b
+                y_line = y_f
+            
+            # Rotation movement adjustments
+            if self.rotate:
                 if leg == "HR" or leg == "FL":
-                    x_arc = x_f
-                    x_line = x_b
+                    y_arc = y_f
+                    y_line = y_b
                 else:
-                    x_arc = x_b
-                    x_line = x_f
+                    y_arc = y_b
+                    y_line = y_f
 
                 if leg == "FR" or leg == "HR":
-                    x_arc = -x_arc
-                    x_line = -x_line
-
+                    y_arc = -y_arc
+                    y_line = -y_line 
+            
+            # Create splines for all directions
             self.splines[leg]["arc"]["x"] = CubicSpline(t_arc_points, x_arc)
+            self.splines[leg]["arc"]["y"] = CubicSpline(t_arc_points, y_arc)
             self.splines[leg]["arc"]["z"] = CubicSpline(t_arc_points, zarc)
+            
             self.splines[leg]["line"]["x"] = CubicSpline(t_arc_points, x_line)
+            self.splines[leg]["line"]["y"] = CubicSpline(t_arc_points, y_line)
             self.splines[leg]["line"]["z"] = CubicSpline(t_arc_points, zline)
 
     def init_pose(self, q, dq):
@@ -179,12 +209,15 @@ class RobotMove(TaskBase):
         z_acc = self.splines[leg][motion]["z"](
             s_t, 2) * (s_dot**2) + self.splines[leg][motion]["z"](s_t, 1) * s_ddot
 
-        if self.move_x:
-            return np.array([x_pos, 0.1469 if "FL" in leg or "HL" in leg else -0.1469, z_pos]), \
-                np.array([x_vel, 0, z_vel]), np.array([x_acc, 0, z_acc])
-        else:
-            return np.array([0.196 if "FL" in leg or "FR" in leg else -0.196, x_pos, z_pos]), \
-                np.array([0, x_vel, z_vel]), np.array([0, x_acc, z_acc])
+        # Get Y position, velocity and acceleration from splines
+        y_pos = self.splines[leg][motion]["y"](s_t)
+        y_vel = self.splines[leg][motion]["y"](s_t, 1) * s_dot
+        y_acc = self.splines[leg][motion]["y"](s_t, 2) * (s_dot**2) + self.splines[leg][motion]["y"](s_t, 1) * s_ddot
+        
+        # Return full 3D trajectory with both X and Y components
+        return np.array([x_pos, y_pos, z_pos]), \
+               np.array([x_vel, y_vel, z_vel]), \
+               np.array([x_acc, y_acc, z_acc])
 
     def compute_control(self, t, position, velocity, sensors):
         if self.off==0:
@@ -214,9 +247,6 @@ class RobotMove(TaskBase):
                 dq,ddq = self.joint_controller.calculate_acceleration(leg,ref_pos,vel,acc)
                 self.ndq += dq
                 self.nddq += ddq
-                # self.logger.info("Current caluclated ddq: {}".format(self.nddq))
 
-            # self.logger.info("FIinal ddq: {}".format(self.nddq))
             tourques = self.joint_controller.get_tourqe(self.ndq,self.nddq)
-            # self.logger.info("Sovle time: {}".format(time.time() - start_time))
             return tourques
