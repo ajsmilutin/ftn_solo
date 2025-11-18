10e3601627745e3172e0c013110c530c39dee675
Tenebram <stefandjekic2001@gmail.com>
2025-11-18 22:25:22 +0100

    Add limitation to avoid singularity and overturn

--- ftn_solo/tasks/task_move_base.py
+++ ftn_solo/tasks/task_move_base.py
@@ -16,6 +16,7 @@ from ftn_solo.utils.types import FrictionCone
 import rclpy
 from sensor_msgs.msg import Joy
 from scipy.linalg import expm
+import math
 
 class Estimator:
     def __init__(self, robot) -> None:
@@ -82,7 +83,7 @@ class Estimator:
         b = np.zeros(self.num_contacts*3+3)
         b[0:3] = alpha*sensors["imu"][0]
         b[3:] = -np.dot(J[3:, 6:], qv)
-        self.estimated_qv[0:6] = np.dot(np.linalg.pinv(J[:, :6]), b)
+        self.estimated_qv[0:6] = np.dot(    np.linalg.pinv(J[:, :6]), b)
 
 
 class TaskMoveBase(TaskBase):
@@ -120,21 +121,27 @@ class TaskMoveBase(TaskBase):
         self.des_linear_velocity = np.array([0.0, 0.0, 0.0])
         self.des_angular_velocity = np.array([0.0, 0.0, 0.0])
         self.previous_t = 0
+        self.inside = True
+        self.distance_from_edge = np.zeros(4)
+        self.scale_up_down_buttons = 1.0
+        self.scale_left_stick_up_down = 1.0
+        self.scale_left_stick_left_right = 1.0
+        self.scale_right_stick = 1.0
 
     def joy_callback(self, msg):
         self.msg = msg
-        if msg.axes[0] > 0.05 or msg.axes[0] < -0.05:
-            self.des_linear_velocity[1] = msg.axes[0] * 0.1
+        if (msg.axes[0] > 0.05 or msg.axes[0] < -0.05):
+            self.des_linear_velocity[1] = msg.axes[0] * 0.15 * self.scale_left_stick_left_right
         else: self.des_linear_velocity[1] = 0
-        if msg.axes[1] > 0.05 or msg.axes[1] < -0.05:
-            self.des_linear_velocity[0] = msg.axes[1] * 0.1
+        if (msg.axes[1] > 0.05 or msg.axes[1] < -0.05):
+            self.des_linear_velocity[0] = msg.axes[1] * 0.15 * self.scale_left_stick_up_down
         else: self.des_linear_velocity[0] = 0
         if msg.axes[3] > 0.05 or msg.axes[3] < -0.05:
-            self.des_angular_velocity[0] = msg.axes[3] * 0.3
+            self.des_angular_velocity[0] = msg.axes[3] * 0.3 * self.scale_right_stick
         else:
             self.des_angular_velocity[0] = 0
         if msg.axes[4] > 0.05 or msg.axes[4] < -0.05:
-            self.des_angular_velocity[1] = -msg.axes[4] * 0.3
+            self.des_angular_velocity[1] = -msg.axes[4] * 0.3 * (1 - self.scale_right_stick)
         else:   
             self.des_angular_velocity[1] = 0
         if msg.buttons[6]:
@@ -143,9 +150,9 @@ class TaskMoveBase(TaskBase):
             self.des_angular_velocity[2] = -msg.buttons[7] * 0.2
         else: self.des_angular_velocity[2] = 0
         if msg.buttons[4]:
-            self.des_linear_velocity[2] = -0.2
+            self.des_linear_velocity[2] = -0.2 * (1 - self.scale_up_down_buttons)#-0.5 * ((1 - 0.15 / self.robot.pin_robot.data.oMf[self.base_index].translation[-1]))
         elif msg.buttons[5]:
-            self.des_linear_velocity[2] = 0.2
+            self.des_linear_velocity[2] = 0.2 * self.scale_up_down_buttons
         else: self.des_linear_velocity[2] = 0
     
     def transition_maker(self):
@@ -174,6 +181,8 @@ class TaskMoveBase(TaskBase):
         self.qp = proxsuite.proxqp.dense.QP(
             self.robot.pin_robot.nv*2-6+4*3, self.robot.pin_robot.nv+4*3, 4*self.num_faces, False, proxsuite.proxqp.dense.HessianType.Diagonal)
         self.des_q = self.estimator.estimated_q
+        self.des_qv = np.zeros(self.robot.pin_robot.nv)
+
         for index in self.robot.end_eff_ids:
             self.friction_cones[index] = FrictionCone(0.8, self.num_faces, 
                 self.robot.pin_robot.data.oMf[index].translation, np.eye(3))
@@ -194,7 +203,24 @@ class TaskMoveBase(TaskBase):
         Kd = 20
         dt = t - self.previous_t
 
-        self.des_qv = np.zeros(self.robot.pin_robot.nv)
+        self.distance_point_to_edge()
+
+        knee_angles = self.get_knee_angles() #fr, fl, hl, hr
+        angle_upper_limit = 0.3
+        angle_lower_limit = 0.9
+
+        self.scale = (knee_angles[2] - angle_upper_limit) / (angle_lower_limit - angle_upper_limit)
+        if self.des_linear_velocity[0] > 0:
+            self.scale_left_stick_up_down = (self.distance_from_edge[0] - 0.05) / (0.2 - 0.05)
+        elif self.des_linear_velocity[0] < 0:
+            self.scale_left_stick_up_down = (self.distance_from_edge[2] - 0.05) / (0.2 - 0.05)
+        
+        if self.des_linear_velocity[1] > 0:
+            self.scale_left_stick_left_right = (self.distance_from_edge[1] - 0.05) / (0.2 - 0.05)
+        elif self.des_linear_velocity[1] < 0:
+            self.scale_left_stick_left_right = (self.distance_from_edge[3] - 0.05) / (0.2 - 0.05)
+
+        # self.node.get_logger().info(str(self.distance_from_edge))
         self.des_qv[0:3] = self.des_linear_velocity
         self.des_qv[3:6] = self.des_angular_velocity
         self.des_q = pin.integrate(self.robot.pin_robot.model, self.des_q, self.des_qv * dt)
@@ -252,6 +278,115 @@ class TaskMoveBase(TaskBase):
         self.previous_t = t
         return True
 
+    def get_knee_angles(self):
+        fr_angle = self.robot.pin_robot.data.oMf[self.robot.fr_index].rotation
+        fr_angle = np.arctan2(-fr_angle[2, 0], np.sqrt(fr_angle[0, 0]**2 + fr_angle[1, 0]**2))
+
+        hr_angle = self.robot.pin_robot.data.oMf[self.robot.hr_index].rotation
+        hr_angle = np.arctan2(-hr_angle[2, 0], np.sqrt(hr_angle[0, 0]**2 + hr_angle[1, 0]**2))
+
+        fl_angle = self.robot.pin_robot.data.oMf[self.robot.fl_index].rotation
+        fl_angle = np.arctan2(-fl_angle[2, 0], np.sqrt(fl_angle[0, 0]**2 + fl_angle[1, 0]**2))
+
+        hl_angle = self.robot.pin_robot.data.oMf[self.robot.hl_index].rotation
+        hl_angle = np.arctan2(-hl_angle[2, 0], np.sqrt(hl_angle[0, 0]**2 + hl_angle[1, 0]**2))
+
+        return np.array([fr_angle, fl_angle, hl_angle, hr_angle])
+
+    def draw_backbone_verticals(self):
+        marker_array = MarkerArray()
+        id = 0
+        for index in (self.robot.hl_index, self.robot.hr_index, self.robot.fl_index, self.robot.fr_index):
+            marker = Marker()
+            marker.header.frame_id = "world"
+            marker.action = Marker.ADD
+            marker.type = Marker.CYLINDER
+            marker.color = ColorRGBA(r=0.0, g=1.0, b=0.0, a=0.5)
+            marker.scale.x = 0.01
+            marker.scale.y = 0.01 
+            marker.scale.z = 0.50
+            marker.pose.position = ToPoint(
+                self.robot.pin_robot.data.oMf[index].translation)
+            marker.id = id
+            id = id + 1
+            marker_array.markers.append(marker)
+        self.publisher.publish(marker_array)
+
+    def distance_from_polygon(self):
+        x, y, z = self.robot.pin_robot.data.com[0]
+        n = 4
+        inside = True
+        
+        legs = (self.robot.fr_index, self.robot.fl_index, self.robot.hl_index, self.robot.hr_index)
+
+        for i in range(n):
+            x1, y1, z1 = self.robot.pin_robot.data.oMf[legs[i]].translation
+            x2, y2, z2 = self.robot.pin_robot.data.oMf[legs[(i + 1) % n]].translation  # Next vertex, wrap around at the end
+
+            # Vector from edge start to edge end
+            edge_vector = np.array([x2 - x1, y2 - y1])
+
+            self.distance_from_edge[i] = (edge_vector[0] * x - edge_vector[1] * y) / (np.sqrt(edge_vector[0]**2 + edge_vector[1]**2))
+        
+        self.node.get_logger().info(str(self.distance_from_edge))
+
+    def is_point_inside_convex_polygon(self):
+
+        x, y, z = self.robot.pin_robot.data.com[0]
+        n = 4
+        inside = True
+        
+        legs = (self.robot.fr_index, self.robot.fl_index, self.robot.hl_index, self.robot.hr_index)
+        i = 0
+        for i in range(n):
+            x1, y1, z1 = self.robot.pin_robot.data.oMf[legs[i]].translation
+            x2, y2, z2 = self.robot.pin_robot.data.oMf[legs[(i + 1) % n]].translation  # Next vertex, wrap around at the end
+
+            # Vector from edge start to edge end
+            edge_vector = np.array([x2 - x1, y2 - y1])
+            # Vector from edge start to the point
+            point_vector = np.array([x - x2, y - y2])
+            
+            # Cross product to determine which side the point is on
+            cross_product = np.cross(edge_vector, point_vector)
+
+            if cross_product < 0.015:  # Point is outside the polygon if cross product is negative
+                inside = False
+                break
+        
+        return inside
+
+    def distance_point_to_edge(self):
+        x, y, z = self.robot.pin_robot.data.com[0]
+        n = 4
+        inside = True
+        
+        legs = (self.robot.fr_index, self.robot.fl_index, self.robot.hl_index, self.robot.hr_index)
+        for i in range(n):
+            x1, y1, z1 = self.robot.pin_robot.data.oMf[legs[i]].translation
+            x2, y2, z2 = self.robot.pin_robot.data.oMf[legs[(i + 1) % n]].translation
+
+            # Vector from edge start to edge end
+            edge_vector = np.array([x2 - x1, y2 - y1])
+            # Vector from edge start to the point
+            point_vector = np.array([x - x1, y - y1])
+
+            magnitude = edge_vector[0]**2 + edge_vector[1]**2
+
+            if magnitude == 0:
+                closest_point = np.array([x1, y1])
+            else:
+                projection = (point_vector[0] * edge_vector[0] + point_vector[1] * edge_vector[1]) \
+                            / magnitude
+                projection = max(0, min(1, projection))
+
+                closest_point = np.array([x1 + projection * edge_vector[0], y1 + projection * edge_vector[1]])
+
+            dx = x - closest_point[0]
+            dy = y - closest_point[1]        
+            self.distance_from_edge[i] = math.sqrt(dx**2 + dy**2)
+        
+
     def compute_control(self, t, q, qv, sensors):
         if (self.step == 0):
             self.estimator.init(q, qv, sensors)
@@ -261,6 +396,11 @@ class TaskMoveBase(TaskBase):
         full_qv = self.estimator.estimated_qv
         self.step = self.step+1
         self.robot.forward_robot(full_q, full_qv)
+
+        pin.centerOfMass(self.robot.pin_robot.model, self.robot.pin_robot.data, full_q, full_qv, True)
+        com = self.robot.pin_robot.data.vcom[0]
+        # todo: make a marker for com and represent it in rviz for better understanding
+
         pin.crba(self.robot.pin_robot.model, self.robot.pin_robot.data, full_q)
         pin.nonLinearEffects(self.robot.pin_robot.model,
                              self.robot.pin_robot.data, full_q, full_qv)
@@ -282,9 +422,22 @@ class TaskMoveBase(TaskBase):
                 id = id + 1
                 marker_array.markers.append(marker)
 
+            marker = Marker()
+            marker.header.frame_id = "world"
+            marker.action = Marker.ADD
+            marker.type = Marker.SPHERE
+            marker.color = ColorRGBA(r=1.0, g=0.0, b=0.0, a=0.5)
+            marker.scale.x = 0.05
+            marker.scale.y = 0.05
+            marker.scale.z = 0.1
+            marker.pose.position = ToPoint(com)
+            marker.id = id
+            id = id + 1
+            marker_array.markers.append(marker)
             self.publisher.publish(marker_array)
 
         rclpy.spin_once(self.node, timeout_sec=0)
+
 
         self.tick(t, q, qv)
         return self.control
